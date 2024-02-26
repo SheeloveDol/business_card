06af6eeb2c68c0b483b4dc7330cbc9956c485d35
Sheelove Dol <siesel26@gmail.com>
2024-02-26 15:42:11 -0800

    Added images folder and created avatar picture

--- lib/main.dart
+++ lib/main.dart
@@ -11,37 +11,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.teal,
-            body: SafeArea(
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      color: Colors.red,
-                      width: 100.0,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          color: Colors.yellow,
-                          width: 100.0,
-                          height: 100.0,
-                        ),
-                        Container(
-                          color: Colors.green,
-                          width: 100.0,
-                          height: 100.0,
-                        ),
-                      ],
-                    ),
-                    Container(
-                      color: Colors.blue,
-                      width: 100.0,
-                    ),
-                  ]),
-            )));
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/ProfileHeadshot.jpeg'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
