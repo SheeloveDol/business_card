da8f7de56d59bda53df0f5d1d8ac02104a6e163c
Sheelove Dol <siesel26@gmail.com>
2024-02-27 17:50:07 -0800

    Swapped containers to Card widgets and added divider

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const CircleAvatar(
                   radius: 50.0,
@@ -41,41 +42,42 @@ class MyApp extends StatelessWidget {
                       color: Colors.white,
                       fontWeight: FontWeight.bold),
                 ),
-                Container(
+                SizedBox(
+                    height: 20.0,
+                    width: 150.0,
+                    child: Divider(color: Colors.teal.shade100)),
+                Card(
                   margin: const EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 55.0),
-                  padding: const EdgeInsets.symmetric(
                       vertical: 10.0, horizontal: 25.0),
-                  color: Colors.white,
-                  child: Row(children: <Widget>[
-                    const Icon(Icons.phone, color: Colors.teal),
-                    const SizedBox(width: 25.0),
-                    Text(
-                      '+1 778 714 2747',
-                      style: TextStyle(
-                        color: Colors.teal[900],
-                        fontSize: 18.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ListTile(
+                      leading: const Icon(Icons.phone, color: Colors.teal),
+                      title: Text(
+                        '+1 778 714 2747',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontSize: 18.0,
+                        ),
                       ),
                     ),
-                  ]),
+                  ),
                 ),
-                Container(
+                Card(
                   margin: const EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 55.0),
-                  padding: const EdgeInsets.symmetric(
                       vertical: 10.0, horizontal: 25.0),
-                  color: Colors.white,
-                  child: Row(children: <Widget>[
-                    const Icon(Icons.email, color: Colors.teal),
-                    const SizedBox(width: 25.0),
-                    Text(
-                      '+1 778 714 2747',
-                      style: TextStyle(
-                        color: Colors.teal[900],
-                        fontSize: 18.0,
-                      ),
-                    ),
-                  ]),
+                  child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListTile(
+                        leading: const Icon(Icons.email, color: Colors.teal),
+                        title: Text(
+                          'sheelovedol1@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal[900],
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      )),
                 ),
               ],
             ),
